500b307fb562d5f548529250618b3473c4e51931
Janak Koradiya <63702103+janakkoradiya@users.noreply.github.com>
2024-12-10 21:48:22 +0530

    fix the path

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 import Header from '@/components/header';
-import styles from '../styles/Home.module.css';
+import styles from './../styles/home.module.css';
 import ProtectedRoute from './protectedRoute';
 // import Footer from '@/components/footer';
 
--- pages/protected/about.js
+++ pages/protected/about.js
@@ -1,4 +1,4 @@
-import styles from '../../styles/About.module.css';
+import styles from './../../styles/about.module.css';
 // import styles from '../styles/About.module.css';
 import { useRouter } from "next/router";
 
